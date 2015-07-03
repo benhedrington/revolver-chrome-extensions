84fe532bb33def1e113892855eca5e76fe548abb
Ben Hedrington <ben.hedrington@gmail..com>
2015-07-03 09:13:51 -0500

    0.7 update thanks to Wayne Goyer
    user: Ben Hedrington <ben.hedrington@gmail..com>
    branch 'default'
    changed revolver_tabs/background_script.js
    changed revolver_tabs/manifest.json
    changed revolver_tabs/options.html
    changed revolver_tabs/options_script.js
    changed revolver_tabs/revolver_t_128.png
    changed revolver_tabs/revolver_t_19.png
    changed revolver_tabs/revolver_t_32.png
    changed revolver_tabs/revolver_t_48.png

--- revolver_tabs/background_script.js
+++ revolver_tabs/background_script.js
@@ -1,164 +1,325 @@
-// Global Variables - When possible pulling form Local Storage set via Options page.
-var activeWindows = new Array();
-var timeDelay = 10000;
-if (localStorage["seconds"]) { timeDelay = (localStorage["seconds"]*1000);}
-var tabReload = true;
-if (localStorage["reload"]) { 
-	if (localStorage["reload"] == 'true') {
-		tabReload = true;
-	} else {
-		tabReload = false;
-	}
+/* global chrome */
+var	tabsManifest = {},
+	settings = {},
+	advSettings = {},
+	windowStatus = {},
+	moverTimeOut = {},
+	listeners = {};	
+// Runs initSettings after it checks for and migrates old settings.
+checkForAndMigrateOldSettings(function(){
+	initSettings();	
+});
+function initSettings(){
+	badgeTabs("default");
+	createBaseSettingsIfTheyDontExist();
+	addEventListeners(function(){
+		autoStartIfEnabled(chrome.windows.WINDOW_ID_CURRENT);
+	});	
 }
-var tabInactive = false;
-if (localStorage["inactive"]) { 
-	if (localStorage["inactive"] == 'true') {
-		tabInactive = true;
-	} else {
-		tabInactive = false;
-	}
+// **** Tab Functionality ****
+// Start revolving the tabs
+function go(windowId) {
+	chrome.tabs.query({"windowId": windowId, "active": true}, function(tab){
+		grabTabSettings(windowId, tab[0], function(tabSetting){
+			setMoverTimeout(windowId, tabSetting.seconds);
+			windowStatus[windowId] = "on";
+			badgeTabs('on', windowId);
+		});	
+	});
 }
-var tabAutostart = false;
-if (localStorage["autostart"]) { 
-	if (localStorage["autostart"] == 'true') {
-		tabAutostart = true;
-	} else {
-		tabAutostart = false;
-	}
+// Stop revolving the tabs
+function stop(windowId) {
+	removeTimeout(windowId);
+	chrome.tabs.query({"windowId": windowId, "active": true}, function(tab){
+		windowStatus[windowId] = "off";
+		badgeTabs('', windowId);
+	});
 }
-var noRefreshList = [];
-if (localStorage["noRefreshList"]) {
-	noRefreshList = JSON.parse(localStorage["noRefreshList"]);
-}
-
-function include(arr,obj) {
-    return (arr.indexOf(obj) != -1);
-}
-
-function activeInWindow(windowId)
-{
-	for(i in activeWindows) {
-		if(activeWindows[i] == windowId) {
-			return true;
-		}
-	}
-}
-
-// Setup Initial Badge Text
-var badgeColor = [139,137,137,137];
-chrome.browserAction.setBadgeBackgroundColor({color: badgeColor});
-
-// Called when the user clicks on the browser action.
-chrome.browserAction.onClicked.addListener(function(tab) {
-	var windowId = tab.windowId
-	if (activeInWindow(windowId)) {
-		stop(windowId);
-	} else {
-		go(windowId);
-	}
-});	
-
-function badgeTabs(windowId, text) {
-	chrome.tabs.getAllInWindow(windowId, function(tabs) {
-		for(i in tabs) {
-			switch (text)
-			{
-			case 'on':
-			  chrome.browserAction.setBadgeText({text:"\u2022"});
-			  chrome.browserAction.setBadgeBackgroundColor({color:[0,255,0,100]});
-			  break;
-			case '':
-			  chrome.browserAction.setBadgeText({text:"\u00D7"});
-			  chrome.browserAction.setBadgeBackgroundColor({color:[255,0,0,100]});
-			  break;
-			default:
-			  chrome.browserAction.setBadgeText({text:""});
-			}
+// Switch to the next tab.
+function activateTab(nextTab) {
+	grabTabSettings(nextTab.windowId, nextTab, function(tabSetting){
+		if(tabSetting.reload && !include(settings.noRefreshList, nextTab.url) && nextTab.url.substring(0,19) != "chrome://extensions"){
+			chrome.tabs.reload(nextTab.id, function(){
+				chrome.tabs.update(nextTab.id, {selected: true}, function(){
+					setMoverTimeout(tabSetting.windowId, tabSetting.seconds);
+				});
+			});
+		} else {
+			// Switch Tab right away
+			chrome.tabs.update(nextTab.id, {selected: true});
+			setMoverTimeout(tabSetting.windowId, tabSetting.seconds);
 		}	
 	});
 }
-
-// Start on a specific window
-function go(windowId) {
-	if (localStorage["seconds"]) { timeDelay = (localStorage["seconds"]*1000);}
-	moverInteval = setInterval(function() { moveTabIfIdle() }, timeDelay);
-        console.log('Starting: timeDelay:'+timeDelay+' reload:'+tabReload+' inactive:'+tabInactive);
-	activeWindows.push(windowId);
-	badgeTabs(windowId, 'on');
-}
-
-// Stop on a specific window
-function stop(windowId) {
-	clearInterval(moverInteval);
-        console.log('Stopped.');
-	var index = activeWindows.indexOf(windowId);
-	if(index >= 0) {
-		activeWindows.splice(index);
-		badgeTabs(windowId, '');
-	}
-}
-
-// Switch Tab URL functionality.
-function activateTab(tab) {
-	if (tabReload && !include(noRefreshList, tab.url)) {
-		// Trigger a reload
-		chrome.tabs.update(tab.id, {url: tab.url, selected: tab.selected}, null);
-		// Add a callback to swich tabs after the reload is complete
-		chrome.tabs.onUpdated.addListener(
-			function activateTabCallback( tabId , info ) {
-		    	if ( info.status == "complete" && tabId == tab.id) {
-					chrome.tabs.onUpdated.removeListener(activateTabCallback);
-		        	chrome.tabs.update(tabId, {selected: true});
-		    	}
-			});
-	} else {
-		// Swich Tab right away
-		chrome.tabs.update(tab.id, {selected: true});
-	}
-}
-
-// Call moveTab if the user isn't actually interacting with the browser
-function moveTabIfIdle() {
-	if (tabInactive) {
+// Call moveTab if the user isn't interacting with the machine
+function moveTabIfIdle(timerWindowId, tabTimeout) {
+	if (settings.inactive) {
 		// 15 is the lowest allowable number of seconds for this call
-		// If you try lower, Chrome complains
 		chrome.idle.queryState(15, function(state) {
 			if(state == 'idle') {
-				moveTab();
+				windowStatus[timerWindowId] = "on";
+				badgeTabs("on", timerWindowId);
+				return moveTab(timerWindowId);
 			} else {
-				//Set "wait" color and log.
-				chrome.browserAction.setBadgeText({text:"\u2022"});
-				chrome.browserAction.setBadgeBackgroundColor({color:[0,0,255,100]});
-				console.log('Browser was active, waiting.');
+				windowStatus[timerWindowId] = "pause";
+				badgeTabs("pause", timerWindowId);
+				return setMoverTimeout(timerWindowId, tabTimeout);
 			}
 		});
 	} else {
-		moveTab();
+		moveTab(timerWindowId);
 	}
 }
-
-// Switches to next URL in manifest, re-requests feed if at end of manifest.
-function moveTab() {
-	for(i in activeWindows) {
-		windowId = activeWindows[i];
-		badgeTabs(windowId, 'on');
-		chrome.tabs.getSelected(windowId, function(currentTab){
-			chrome.tabs.getAllInWindow(currentTab.windowId, function(tabs) {
+// Switches to next tab in the index, re-requests feed if at end of the index.
+function moveTab(timerWindowId) {
+	var nextTabIndex = 0;
+	chrome.tabs.getSelected(timerWindowId, function(currentTab){
+		chrome.tabs.getAllInWindow(timerWindowId, function(tabs) {
+			if(currentTab.index + 1 < tabs.length) {
+				nextTabIndex = currentTab.index + 1;
+			} else {
 				nextTabIndex = 0;
-				if(currentTab.index + 1 < tabs.length) {
-					nextTabIndex = currentTab.index + 1;
-				}
-				activateTab(tabs[nextTabIndex]);
+			}
+			activateTab(tabs[nextTabIndex]);
+		});
+	});
+}
+// **** Event Listeners ****
+// Creates all of the event listeners to start/stop the extension and ensure badge text is up to date.
+function addEventListeners(callback){
+	chrome.browserAction.onClicked.addListener(function(tab) {
+		var windowId = tab.windowId;
+		if (windowStatus[windowId] == "on" || windowStatus[windowId] == "pause") {
+			stop(windowId);
+		} else {
+			createTabsManifest(windowId, function(){
+				go(windowId);
 			});
+		}
+	});	
+	chrome.windows.onRemoved.addListener(
+		listeners.onWindowRemoved = function(windowId){
+			removeTimeout(windowId);
+			delete moverTimeOut[windowId];
+			delete windowStatus[windowId];
+			delete tabsManifest[windowId];
+		}
+	);
+	chrome.tabs.onCreated.addListener(
+		listeners.onCreated = function (tab){
+			createTabsManifest(tab.windowId, function(){
+				setBadgeStatusOnActiveWindow(tab);	
+			});
+		}
+	);
+	chrome.tabs.onUpdated.addListener(
+		listeners.onUpdated = function onUpdated(tabId, changeObj, tab){
+			setBadgeStatusOnActiveWindow(tab);
+			if(changeObj.url) createTabsManifest(tab.windowId, function(){
+				return true;
+			});
+		}
+	);
+	chrome.tabs.onActivated.addListener(
+		listeners.onActivated = function(tab){
+			checkIfWindowExists(tab.windowId, function(windowExists){
+				if (windowExists == true) setBadgeStatusOnActiveWindow(tab);
+			});
+		}
+	);
+	chrome.tabs.onAttached.addListener(
+		listeners.onAttached = function(tabId, newWindow){
+			createTabsManifest(newWindow.newWindowId, function(){
+				return true;
+			});
+		}
+	);
+	chrome.tabs.onDetached.addListener(
+		listeners.onDetached = function(tabId, detachWindow){
+			createTabsManifest(detachWindow.oldWindowId, function(){
+				return true;
+			});
+		}
+	);
+	chrome.tabs.onRemoved.addListener(
+		listeners.onRemoved = function(tabId, removedInfo){
+			if(!removedInfo.isWindowClosing){
+				createTabsManifest(removedInfo.windowId, function(){
+					return true;
+				});	
+			}
+		}
+	);
+	chrome.windows.onCreated.addListener(
+		listeners.onWindowCreated = function(window){
+			autoStartIfEnabled(window.id);
+		}
+	);
+	return callback();
+};
+// **** Badge Status ****
+// If the window has revolver tabs enabled, make sure the badge text reflects that.
+function setBadgeStatusOnActiveWindow(tab){
+	if (windowStatus[tab.windowId] === "on") badgeTabs("on", tab.windowId);
+	else if (windowStatus[tab.windowId] === "pause") badgeTabs("pause", tab.windowId);
+	else badgeTabs("", tab.windowId);
+}
+//Change the badge icon/background color.  
+function badgeTabs(text, windowId) {
+	if(text === "default") {
+		chrome.browserAction.setBadgeText({text:"\u00D7"}); //Letter X
+ 		chrome.browserAction.setBadgeBackgroundColor({color:[255,0,0,100]}); //Red	
+	} else {
+		chrome.tabs.query({"windowId": windowId, "active": true}, function(tab){
+			if(text === "on") {
+				chrome.browserAction.setBadgeText({text:"\u2022", tabId: tab[0].id}); //Play button
+		  		chrome.browserAction.setBadgeBackgroundColor({color:[0,255,0,100], tabId: tab[0].id}); //Green
+			} else
+			if (text === "pause"){
+				chrome.browserAction.setBadgeText({text:"\u2022", tabId: tab[0].id}); //Play button
+				chrome.browserAction.setBadgeBackgroundColor({color:[255,238,0,100], tabId: tab[0].id}); //Yellow
+			} else {
+				chrome.browserAction.setBadgeText({text:"\u00D7", tabId: tab[0].id}); //Letter X
+		 		chrome.browserAction.setBadgeBackgroundColor({color:[255,0,0,100], tabId: tab[0].id}); //Red
+			}
+		});	
+	}	
+}		
+// **** Timeouts ***
+// Generate the timeout and assign it to moverTimeOut object.
+function setMoverTimeout(timerWindowId, seconds){
+	moverTimeOut[timerWindowId] = setTimeout(function(){
+		removeTimeout(timerWindowId);
+		moveTabIfIdle(timerWindowId, seconds);
+	}, parseInt(seconds)*1000);
+}
+// Remove the timeout specified.
+function removeTimeout(windowId){
+	clearTimeout(moverTimeOut[windowId]);
+	moverTimeOut[windowId] = "off";
+}
+// **** Helpers ****
+// If a user closes a window, chrome activates each tab (presumably to close them).  This prevents errors when the onActivated listener 
+// is fired on the tabs being activated to close them.
+function checkIfWindowExists(windowId, callback){		
+	chrome.windows.getAll(function(windows){		
+		for(var i=0;i<windows.length;i++){		
+			if(windows[i].id === windowId){		
+				return callback(true);		
+			}		
+		}		
+		return callback(false);		
+	});		
+}
+// Checks if a string exists in an array.
+function include(arr,url) {
+    return (arr.indexOf(url) != -1);
+}
+// Returns all the tabs for the current window.
+function getAllTabsInCurrentWindow(callback){
+	chrome.tabs.query({windowId: chrome.windows.WINDOW_ID_CURRENT}, function(tabs){
+		callback(tabs);
+	});
+}
+// **** Settings ****
+// Checks each tab object for settings, if they don't exist assign them to the object.
+function assignBaseSettings(tabs, callback) {
+	for(var i = 0;i<tabs.length;i++){
+		tabs[i].reload = (tabs[i].reload || settings.reload);
+		tabs[i].seconds = (tabs[i].seconds || settings.seconds);	
+	};
+	callback();
+}
+// If there are advanced settings for the URL, set them to the tab.
+function assignAdvancedSettings(tabs, callback) {
+	for(var y=0;y<tabs.length;y++){
+		for(var i=0;i<advSettings.length;i++){
+			if(advSettings[i].url == tabs[y].url) {
+				tabs[y].reload = advSettings[i].reload;
+				tabs[y].seconds = advSettings[i].seconds;
+			}
+		}	
+	}
+	callback();
+}
+// Get the settings for a tab.
+function grabTabSettings(windowId, tab, callback) {
+	for(var i=0; i<tabsManifest[windowId].length; i++){
+		if(tabsManifest[windowId][i].url === tab.url){
+			return callback(tabsManifest[windowId][i]);
+		}
+	}
+}
+// This will convert users old settings into the new object format and remove the old ones.
+function checkForAndMigrateOldSettings(callback){
+	if(localStorage["revolverSettings"]) callback();
+	else {
+		var oldSettings = ["seconds", "autostart", "inactive", "noRefreshList", "reload"],
+			tempSettings = {};
+		for(var i=0;i<oldSettings.length;i++){
+			if(localStorage[oldSettings[i]]) {
+				tempSettings[oldSettings[i]] = localStorage[oldSettings[i]];
+				delete localStorage[oldSettings[i]];
+			}
+		}
+		if(JSON.stringify(tempSettings) != "{}"){
+			localStorage["revolverSettings"] = JSON.stringify(tempSettings);	
+		}
+		callback();
+	}
+}
+// Check if the objects exist in local storage, create them if they don't, load them if they do.
+function createBaseSettingsIfTheyDontExist(){
+	if(!localStorage["revolverSettings"]){
+		settings.seconds = 15;
+		settings.reload = false;
+		settings.inactive = false;
+		settings.autoStart = false;
+		localStorage["revolverSettings"] = JSON.stringify(settings);
+	} else {
+		settings = JSON.parse(localStorage["revolverSettings"]);
+	};
+	if(localStorage["revolverAdvSettings"]){
+		advSettings = JSON.parse(localStorage["revolverAdvSettings"]);
+	}
+	return true;
+}
+// If user has auto start enabled, well then, auto start.
+function autoStartIfEnabled(windowId){
+	if(settings.autostart) {
+		createTabsManifest(windowId, function(){
+			go(windowId);
 		});
 	}
 }
-//Autostart function, procesed on initial startup.
-if(tabAutostart) {
-	chrome.tabs.query({'active': true, 'windowId': chrome.windows.WINDOW_ID_CURRENT},
-		function(tabs){
-			//Start Revolver Tabs in main window.
-			go(tabs[0].windowId);
-		}
-	);
+// Go through each tab and assign settings to them.
+function assignSettingsToTabs(tabs, callback){
+	assignAdvancedSettings(tabs, function(){
+		assignBaseSettings(tabs, function(){
+			callback();
+		});	
+	});
+}
+// Create the tabs object with settings in tabsManifest object.
+function createTabsManifest(windowId, callback){
+	chrome.tabs.query({"windowId" : windowId}, function(tabs){
+		assignSettingsToTabs(tabs, function(){
+			tabsManifest[windowId] = tabs;
+			callback();
+		});
+	});
+}
+//If a user changes settings this will update them on the fly.  Called from options_script.js
+function updateSettings(){
+	settings = JSON.parse(localStorage["revolverSettings"]);
+	advSettings = JSON.parse(localStorage["revolverAdvSettings"]);
+	getAllTabsInCurrentWindow(function(tabs){
+		assignBaseSettings(tabs, function(){
+			assignAdvancedSettings(tabs, function(){
+				createTabsManifest(tabs[0].windowId, function(){
+					return true;	
+				});
+			});
+		});
+	});
 }
--- revolver_tabs/options_script.js
+++ revolver_tabs/options_script.js
@@ -1,83 +1,186 @@
+/* global chrome */
 var bg = chrome.extension.getBackgroundPage();
 // Saves options to localStorage.
-function save_options() {
-        localStorage["seconds"] = document.getElementById("seconds").value;
-        bg.timeDelay = (document.getElementById("seconds").value*1000);
-        if (document.getElementById("reload").checked == true) {
-                localStorage["reload"] = 'true';
-                bg.tabReload = true;
-        } else {
-                localStorage["reload"] = 'false';
-                bg.tabReload = false;
-        }
-        if (document.getElementById("inactive").checked == true) {
-                localStorage["inactive"] = 'true';
-                bg.tabInactive = true;
-        } else {
-                localStorage["inactive"] = 'false';
-                bg.tabInactive = false;
-        }
-	if (document.getElementById("autostart").checked == true) {
-                localStorage["autostart"] = 'true';
-                bg.tabInactive = true;
-        } else {
-                localStorage["autostart"] = 'false';
-                bg.tabInactive = false;
-        }
-	localStorage["noRefreshList"] = JSON.stringify(document.getElementById('noRefreshList').value.split('\n'));
-        bg.noRefreshList = document.getElementById('noRefreshList').value.split('\n');
-  // Update status to let user know options were saved.
-  var status = document.getElementById("status");
-  var status2 = document.getElementById("status2");
-  status.innerHTML = "OPTIONS SAVED";
-  status2.innerHTML = "OPTIONS SAVED";
-  setTimeout(function() {
-    status.innerHTML = "";
-    status2.innerHTML = "";
-  }, 1000);
+function addEventListeners(){
+    restoreOptions();
+    if (localStorage["revolverAdvSettings"]) restoreAdvancedOptions();
+    buildCurrentTabsList();
+    document.querySelector('#save').addEventListener('click', saveAllOptions);
 }
+
+//Base options code
+function saveBaseOptions(callback) {
+    var appSettings = {},
+        status = document.getElementById("status");
+    appSettings.seconds = document.getElementById("seconds").value;
+    bg.timeDelay = (document.getElementById("seconds").value*1000);
+    getCheckedStatus(appSettings, "reload");
+    getCheckedStatus(appSettings, "inactive");
+    getCheckedStatus(appSettings, "autostart");
+    appSettings.noRefreshList = document.getElementById('noRefreshList').value.split('\n');
+    bg.noRefreshList = document.getElementById('noRefreshList').value.split('\n');  
+    status.innerHTML = "OPTIONS SAVED";
+    setTimeout(function() {
+        status.innerHTML = "";
+  }, 1000);
+  localStorage["revolverSettings"] = JSON.stringify(appSettings);
+  callback();
+}
+
+function getCheckedStatus(appSettings, elementId){
+    if(document.getElementById(elementId).checked){
+        appSettings[elementId] = true;
+    } else {
+        appSettings[elementId] = false;
+    }
+    bg[elementId] = appSettings[elementId];
+}
+
 // Restores saved values from localStorage.
-function restore_options() {
-        if (localStorage["seconds"]) {
-                document.getElementById("seconds").value = localStorage["seconds"];
+function restoreOptions() {
+    var appSettings = {};
+    if (localStorage["revolverSettings"]) appSettings = JSON.parse(localStorage["revolverSettings"]);
+        document.getElementById("seconds").value = (appSettings.seconds || 10);
+        document.getElementById("reload").checked = (appSettings.reload || false);
+        document.getElementById("inactive").checked = (appSettings.inactive || false);
+        document.getElementById("autostart").checked = (appSettings.autostart || false);
+        if(appSettings.noRefreshList && appSettings.noRefreshList.length > 0){
+            for(var i=0;i<appSettings.noRefreshList.length;i++){
+                if(appSettings.noRefreshList[i]!= ""){
+                    document.getElementById("noRefreshList").value += (appSettings.noRefreshList[i]+"\n");    
+                };
+            };
         } else {
-                document.getElementById("seconds").value = "10";
-        }
-        if (localStorage["reload"]) {
-                if (localStorage["reload"] == 'true') {
-                        document.getElementById("reload").checked = true;
-                } else {
-                        document.getElementById("reload").checked = false;
-                }
-        } else {
-                document.getElementById("reload").checked = true;
-        }
-        if (localStorage["inactive"]) {
-                if (localStorage["inactive"] == 'true') {
-                        document.getElementById("inactive").checked = true;
-                } else {
-                        document.getElementById("inactive").checked = false;
-                }
-        } else {
-                document.getElementById("inactive").checked = true;
-        }
-	if (localStorage["autostart"]) {
-                if (localStorage["autostart"] == 'true') {
-                        document.getElementById("autostart").checked = true;
-                } else {
-                        document.getElementById("autostart").checked = false;
-                }
-        } else {
-                document.getElementById("autostart").checked = false;
-        }
-        if (localStorage["noRefreshList"]) {
-                document.getElementById("noRefreshList").value = JSON.parse(localStorage["noRefreshList"]).join("\n");
-        } else {
-                document.getElementById("noRefreshList").value = "";
+            document.getElementById("noRefreshList").value = "";    
         }
 }
 
-// Adding listeners for restoring and saving options
-document.addEventListener('DOMContentLoaded', restore_options);
-document.querySelector('#save').addEventListener('click', save_options);
-document.querySelector('#savetop').addEventListener('click', save_options);
+//Advanced options code
+function saveAdvancedOptions(callback){
+    var advUrlObjectArray = [],
+        advancedSettings = document.getElementById("adv-settings"),
+        advancedDivs = advancedSettings.getElementsByTagName("div"),
+        status = document.getElementById("status3"),
+        divInputTags;
+        for(var i = 0, checkboxes=0;i<advancedDivs.length;i++){
+           if(advancedDivs[i].getElementsByClassName("enable")[0].checked == true){
+               divInputTags = advancedDivs[i].getElementsByTagName("input");
+                advUrlObjectArray.push({
+                    "url" : advancedDivs[i].getElementsByClassName("url-text")[0].value,
+                    "reload" : divInputTags[3].checked,
+                    "seconds" : divInputTags[2].value,
+                    "favIconUrl": advancedDivs[i].getElementsByClassName("icon")[0].src
+                });               
+           }
+        }
+        localStorage["revolverAdvSettings"] = JSON.stringify(advUrlObjectArray);
+        bg.updateSettings();
+        status.innerHTML = "OPTIONS SAVED";
+        setTimeout(function() {
+            status.innerHTML = "";
+         }, 1000);
+        callback();
+}
+
+function restoreAdvancedOptions(){
+    var settings = JSON.parse(localStorage["revolverAdvSettings"]);
+    if(settings.length>0){
+        for(var i=0;i<settings.length;i++){
+            generateAdvancedSettingsHtml(settings[i], true);
+        }    
+    }
+}
+
+function generateAdvancedSettingsHtml(tab, saved){
+    var advancedSettings = document.getElementsByClassName("adv-settings")[0],
+        enableHtmlChunk = '<div><input type="checkbox" class="enable" name="enable">',
+        iconAndUrlChunk = '<img class="icon" src='+tab.favIconUrl+'\><input class="url-text" type="text" value="'+tab.url+'">',
+        secondsChunk = '<p><label for="seconds">Seconds:</label> <input type="text" name="seconds" value="10" style="width:30px;">',
+        reloadChunk = '<label class="inline" for="reload">Reload:</label> <input type="checkbox" name="reload"></p></div>';
+        if(saved){ 
+            enableHtmlChunk = '<div><input type="checkbox" class="enable" name="enable" checked>';
+            secondsChunk = '<p><label for="seconds">Seconds:</label> <input type="text" name="seconds" value="'+tab.seconds+'" style="width:30px;">';
+            if(tab.reload){
+                reloadChunk = '<label class="inline" for="reload">Reload:</label> <input type="checkbox" name="reload" checked></p></div>';    
+            } 
+        }
+        advancedSettings.innerHTML += enableHtmlChunk + iconAndUrlChunk + secondsChunk + reloadChunk;
+};
+
+function getCurrentTabs(callback){
+    var returnTabs=[];
+    chrome.windows.getCurrent({populate: true}, function(window){
+        window.tabs.forEach(function(tab){
+          if(tab.url.substring(0,16) != "chrome-extension"){
+              returnTabs.push(tab);
+          }
+       });
+       callback(returnTabs);
+    });
+}
+
+function buildCurrentTabsList(){ 
+    getCurrentTabs(function(allCurrentTabs){
+        if(localStorage["revolverAdvSettings"]){
+        compareSavedAndCurrentUrls(function(urls){
+            for(var i=0;i<urls.length;i++){
+                for(var y=0;y<allCurrentTabs.length;y++){
+                    if(urls[i] === allCurrentTabs[y].url){
+                        generateAdvancedSettingsHtml(allCurrentTabs[y]);
+                    }
+                }
+            } 
+            createAdvancedSaveButton();
+        });    
+        } else {
+            allCurrentTabs.forEach(function(tab) {
+                generateAdvancedSettingsHtml(tab);
+            });
+            createAdvancedSaveButton();
+        }
+    });
+}
+
+function compareSavedAndCurrentUrls(callback){
+    var currentTabsUrls = [],
+        savedTabsUrls = [],
+        urlsToWrite = [];
+        
+    JSON.parse(localStorage["revolverAdvSettings"]).forEach(function(save){
+       savedTabsUrls.push(save.url); 
+    });
+    getCurrentTabs(function(allCurrentTabs){
+       for(var i=0;i<allCurrentTabs.length;i++){
+         currentTabsUrls.push(allCurrentTabs[i].url);
+       };
+       for(var i=0;i<currentTabsUrls.length;i++){
+            if(savedTabsUrls.indexOf(currentTabsUrls[i]) == -1){
+                urlsToWrite.push(currentTabsUrls[i]);        
+            }
+       };
+       callback(urlsToWrite);
+    });
+}
+
+function saveAllOptions(){
+    saveBaseOptions(function(){
+       saveAdvancedOptions(function(){
+          return true; 
+       });
+    });
+}
+
+function createAdvancedSaveButton(){
+    var parent = document.querySelector("#adv-settings"),
+        advSaveButton = document.createElement("button"),
+        advSaveIndicator = document.createElement("span");
+    advSaveButton.setAttribute("id", "adv-save");
+    advSaveButton.innerText = "Save";
+    advSaveButton.addEventListener("click", saveAllOptions);
+    advSaveIndicator.setAttribute("id", "status3");
+    parent.appendChild(advSaveButton);
+    parent.appendChild(advSaveIndicator); 
+}
+
+// Load settings and add listeners:
+document.addEventListener('DOMContentLoaded', addEventListeners);
